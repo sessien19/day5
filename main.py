2e82231cdfc9c33b3e966301b13be10a48043424
Samuel Essien <samuelg2v@outlook.com>
2024-11-26 14:48:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Marvel movie character creator")
+print()
+spiderman = input("Do you like hanging around? ")
+if spiderman == "yes":
+  print("You are spiderman")
+
+hulk = input("Are you strong")
+if hulk == "yes":
+  print("You are hulk")
+
+deadpool = input("Are you a crazy person? ")
+if deadpool == "yes":
+  print("You are deadpool")
+else:
+  print("You are not a superhero")
